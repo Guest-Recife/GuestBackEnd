540addb284dc86bdde209b1494adc8fd61ab7e29
LucaDRF <lucaroldao11@gmail.com>
2022-11-21 03:20:23 -0300

    feat(user): schema improvement

--- server/src/schemas/user.js
+++ server/src/schemas/user.js
@@ -5,6 +5,8 @@ import {
   date
 } from 'yup';
 
+import sla from '../utils/yup';
+
 const schemas = {
   login: {
     body: object().shape({
@@ -17,15 +19,42 @@ const schemas = {
       last_name: string().min(1).max(99).required(),
       birth_date: date().required(),
       gender: string().required().oneOf(['masculino', 'feminino', 'outro']),
-      cpf: number().min(1),
-      phone: number().min(1).required(),
+      cpf: string()
+        .matches(/^[0-9]+$/, 'Must be only digits')
+        .min(11, 'Must be exactly 11 digits')
+        .max(11, 'Must be exactly 11 digits'),
+      phone: string().required()
+        .matches(/^[0-9]+$/, 'Must be only digits')
+        .min(11, 'Must be exactly 11 digits')
+        .max(11, 'Must be exactly 11 digits'),
       email: string().email().required(),
       password: string().required()
     }).noUnknown()
+  }, update: {
+    body: object().shape({
+      file_id: number().min(1),
+      name: string().min(1).max(99),
+      last_name: string().min(1).max(99),
+      birth_date: date(),
+      gender: string().oneOf(['masculino', 'feminino', 'outro']),
+      cpf: string().test('test-invalid-cpf', 'cpf inválido', sla.isCPFValid())
+        .matches(/^[0-9]+$/, 'Must be only digits')
+        .min(11, 'Must be exactly 11 digits')
+        .max(11, 'Must be exactly 11 digits'),
+      phone: string()
+        .matches(/^[0-9]+$/, 'Must be only digits')
+        .min(11, 'Must be exactly 11 digits')
+        .max(11, 'Must be exactly 11 digits'),
+      email: string().email(),
+      password: string().min(6)
+        .when(['new_password'], (new_password, schema) => new_password ? schema.required() : schema),
+      new_password: string().min(6)
+    })
   }
 };
 
 export default {
   login: object(schemas.login),
-  register: object(schemas.register)
+  register: object(schemas.register),
+  update: object(schemas.update)
 };
