4936dced04540f1d9cb7d2ea3de44094b5cdd1d8
LucaDRF <lucaroldao11@gmail.com>
2022-11-22 19:47:26 -0300

    fix(): improvements

--- server/src/migrations/20220801123257-add-users-table.js
+++ server/src/migrations/20220801123257-add-users-table.js
@@ -1,11 +1,59 @@
 'use strict';
 
 module.exports = {
-  async up(queryInterface) {
+  async up(queryInterface, Sequelize) {
     const transaction = await queryInterface.sequelize.transaction();
 
     try {
-      await queryInterface.sequelize.query('CREATE EXTENSION citext;', { transaction });
+      await queryInterface.createTable('users', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        name: {
+          allowNull: false,
+          type: Sequelize.DataTypes.STRING
+        },
+        last_name: {
+          allowNull: false,
+          type: Sequelize.DataTypes.STRING
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.DataTypes.STRING
+        },
+        birth_date: {
+          allowNull: false,
+          type: Sequelize.DataTypes.DATE
+        },
+        email: 'citext UNIQUE NOT NULL',
+        gender: {
+          allowNull: false,
+          type: Sequelize.DataTypes.STRING
+        },
+        cpf: {
+          allowNull: true,
+          type: Sequelize.DataTypes.STRING
+        },
+        phone: {
+          allowNull: true,
+          type: Sequelize.DataTypes.STRING
+        },
+        created_at: {
+          type: Sequelize.DataTypes.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        },
+        deleted_at: {
+          type: Sequelize.DataTypes.DATE,
+          allowNull: true
+        }
+      });
 
       await transaction.commit();
     } catch (error) {
@@ -18,7 +66,7 @@ module.exports = {
     const transaction = await queryInterface.sequelize.transaction();
 
     try {
-      await queryInterface.sequelize.query('DROP EXTENSION citext;', { transaction });
+      await queryInterface.dropTable('users');
 
       await transaction.commit();
     } catch (error) {
